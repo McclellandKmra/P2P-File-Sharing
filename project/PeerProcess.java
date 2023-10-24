d3a47bcb35ac14483e1ae9c7dd19eba760d7b6be
LandonWh <whitel@ufl.edu>
2023-10-23 21:52:17 -0400

    logic for choking and unchoking

--- project/PeerProcess.java
+++ project/PeerProcess.java
@@ -6,7 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class PeerProcess {
-    public Thread t1;
+    public Thread t1, t2;
     private int numberOfPreferredNeighbors;
     private int unchokingInterval;
     private int optimisticUnchokingInterval;
@@ -16,14 +16,18 @@ public class PeerProcess {
     private int peerID;
 
     private BitSet bitfield;
+    private HashMap<Socket, BitSet> peerBitfields = new HashMap<>();
 
     private Map<Integer, PeerInfo> peers = new HashMap<>();
+    private Map<Socket, Integer> peerIDs = new HashMap<>();
 
     private ServerSocket serverSocket;
     private HashMap<Socket, ObjectOutputStream> objectOutputStreams = new HashMap<>();
     private HashMap<Socket, ObjectInputStream> objectInputStreams = new HashMap<>();
-    private List<Socket> incomingConnections = new ArrayList<>();
-    private List<Socket> outgoingConnections = new ArrayList<>();
+    private List<Socket> connections = new ArrayList<>();
+    private List<Socket> preferredNeighbors = new ArrayList<>();
+    private Set<Socket> interestedPeers = new HashSet<>();
+
     private List<Thread> listenerThreads = new ArrayList<>();
 
     public class PeerInfo {
@@ -57,6 +61,11 @@ public class PeerProcess {
         peerProcess.t1.start();
 
         peerProcess.connectToServers();
+
+        peerProcess.t2 = new Thread(()-> {
+            peerProcess.unchokingInterval();
+        });
+        peerProcess.t2.start();
 
 
         
@@ -126,7 +135,7 @@ public class PeerProcess {
         bitfield = new BitSet(totalPieces);
 
         if (currentPeer.hasFile) {
-            bitfield.set(0, totalPieces);  // Set all bits to 1 if the peer has the complete file
+            bitfield.set(0, totalPieces);// Set all bits to 1 if the peer has the complete file
         } else {
             bitfield.clear();  // Clear all bits (set to 0) if the peer does not have the complete file
         }
@@ -148,7 +157,7 @@ public class PeerProcess {
                 Socket socket = serverSocket.accept();
                 objectOutputStreams.put(socket, new ObjectOutputStream(socket.getOutputStream()));
                 objectInputStreams.put(socket, new ObjectInputStream(socket.getInputStream()));
-                incomingConnections.add(socket);
+                connections.add(socket);
                 sendHandshake(socket);
                 receiveHandshake(socket);
                 Thread listenerThread = new Thread(() -> listenForMessages(socket));
@@ -162,6 +171,8 @@ public class PeerProcess {
             e.printStackTrace();
         }
     }
+
+    //Messages
 
     public void sendHandshake(Socket socket) {
         try {
@@ -229,7 +240,7 @@ public class PeerProcess {
                     // Store these streams in the appropriate maps
                     objectOutputStreams.put(socket, out);
                     objectInputStreams.put(socket, in);
-                    incomingConnections.add(socket);
+                    connections.add(socket);
     
                     // Send a handshake message to the connected peer
                     sendHandshake(socket);
@@ -274,15 +285,69 @@ public class PeerProcess {
     public void handleMessage(Socket socket, byte[] message) {
         int messageType = Byte.toUnsignedInt(message[4]);
         switch(messageType) {
-            case 5: handleBitfieldMessage(socket, message);
+            case 0: handleChokeMessage(socket, message); break;
+            case 1: handleUnchokeMessage(socket, message); break;
+            case 3: handleNotInterestedMessage(socket, message); break;
+            case 2: handleInterestedMessage(socket, message); break;
+            case 5: handleBitfieldMessage(socket, message); break;
         }
     }
 
     public void handleBitfieldMessage(Socket socket, byte[] message) {
-        System.out.println("Received bitfield message from " + socket.getRemoteSocketAddress());
+        // Extract the bitfield part of the message, skipping the message length and type bytes.
+        byte[] bitfieldBytes = Arrays.copyOfRange(message, 5, message.length);
+        
+        BitSet receivedBitfield = byteArrayToBitset(bitfieldBytes);
+        peerBitfields.put(socket, receivedBitfield);
+    
+        // Convert the received bitfield to a string of ones and zeros
+        StringBuilder bitfieldString = new StringBuilder(receivedBitfield.length());
+        for (int i = 0; i < receivedBitfield.length(); i++) {
+            if (receivedBitfield.get(i)) {
+                bitfieldString.append("1");
+            } else {
+                bitfieldString.append("0");
+            }
+        }
+    
+        // Print the received bitfield
+        System.out.println("Received bitfield from " + socket.getRemoteSocketAddress() + ": " + bitfieldString.toString());
+        
+        // Check if the received bitfield has pieces that this peer doesn't have
+        BitSet missingPieces = (BitSet) receivedBitfield.clone();
+        missingPieces.andNot(this.bitfield);
+        
+        if (missingPieces.isEmpty()) {
+            sendNotInterestedMessage(socket);
+        } else {
+            sendInterestedMessage(socket);
+        }
+    }
+      
+    public void sendMessage(Socket socket, byte messageType, byte[] payload) {
+        try {
+            ObjectOutputStream out = objectOutputStreams.get(socket);
+    
+            // Determine message length
+            int messageLength = (payload != null) ? payload.length + 1 : 1;
+    
+            // Create message
+            ByteBuffer messageBuffer = ByteBuffer.allocate(4 + messageLength);
+            messageBuffer.putInt(messageLength);
+            messageBuffer.put(messageType);
+            if (payload != null) {
+                messageBuffer.put(payload);
+            }
+    
+            // Send message
+            out.writeObject(messageBuffer.array());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void sendBitfieldMessage(Socket socket) {
+        //TODO: use send message function
         try {
             ObjectOutputStream out = objectOutputStreams.get(socket);
             
@@ -314,6 +379,98 @@ public class PeerProcess {
         return bytes;
     }
 
+    private BitSet byteArrayToBitset(byte[] bytes) {
+        BitSet bitset = new BitSet(bytes.length * 8);
+        
+        for (int i = 0; i < bytes.length * 8; i++) {
+            if ((bytes[i / 8] & (1 << (7 - i % 8))) != 0) {
+                bitset.set(i);
+            }
+        }
+        return bitset;
+    }
+
+    private void sendNotInterestedMessage(Socket socket) {
+        sendMessage(socket, (byte) 3, null);
+    }
+
+    private void handleNotInterestedMessage(Socket socket, byte[] message) {
+        interestedPeers.remove(socket);
+        //TODO: handle not interested message and log
+        System.out.println("received not interested message from " + socket.getRemoteSocketAddress());
+    }
+
+    private void sendInterestedMessage(Socket socket) {
+        sendMessage(socket, (byte) 2, null);
+    }
+
+    private void handleInterestedMessage(Socket socket, byte[] message) {
+        interestedPeers.add(socket);
+        //TODO: handle interested messaage and log
+        System.out.println("received interested message from " + socket.getRemoteSocketAddress());
+    }
+
+    private void sendUnchokeMessage(Socket socket) {
+        sendMessage(socket, (byte) 1, null);
+        System.out.println("Sent unchoke message to " + socket.getRemoteSocketAddress());
+    }
+
+    //TODO:
+    private void handleUnchokeMessage(Socket socket, byte[] message) {
+        System.out.println("getting choked by " + socket.getRemoteSocketAddress());
+    }
+
+    private void sendChokeMessage(Socket socket) {
+        sendMessage(socket, (byte) 0, null);
+        System.out.println("Sent choke message to " + socket.getRemoteSocketAddress());
+    }
+
+    //TODO:
+    private void handleChokeMessage(Socket socket, byte[] message) {
+        System.out.println("getting unchoked by " + socket.getRemoteSocketAddress());
+    }
+
+    //Choking
+
+    public void unchokingInterval() {
+        while (true) { // Infinite loop to keep checking at regular intervals
+            preferredNeighbors.clear();
+            if (peers.get(peerID).hasFile) { //choose preferred neighbors randomly
+                List<Socket> interested = new ArrayList<>(interestedPeers); // Assuming you have this list from before
+                Collections.shuffle(interested);
+                
+                preferredNeighbors.clear();
+                for (int i = 0; i < Math.min(numberOfPreferredNeighbors, interested.size()); i++) {
+                    Socket neighbor = interested.get(i);
+                    preferredNeighbors.add(neighbor);
+                    unchoke(neighbor);
+                }
+                chokeNonPreferredNeighbors();
+            }
+            //TODO: implement else when peer does not have complete file
     
+            try {
+                Thread.sleep(1000 * unchokingInterval);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void unchoke(Socket socket) {
+        //TODO: logic for making sure it wasnt previously choked
+        sendUnchokeMessage(socket);
+    }
+
+    public void chokeNonPreferredNeighbors() {
+        for (Socket socket : connections) { //TODO: optimistically unchoked neighbor logic
+            if (!preferredNeighbors.contains(socket)) {
+                sendChokeMessage(socket);
+            }
+        }
+    }
+
+
+
 }
 
